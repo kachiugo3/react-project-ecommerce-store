7468c2cdedae6cd732b4dd632a7da8dd6f57334f
kachiugo3 <kachiugo3@gmail.com>
2023-07-28 11:37:36 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Notes
+
+Welcome to my complete e-commerce app project.
+
+This is a complete, large scale e-commerce app built using React.js.
+
+Functionalities include:
+
+- Sleek and intuitive design
+- Cart functionality
+- Authentication with Private Routes
+- Stripe integration
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  AboutPage,
+  HomePage,
+  AuthWrapper,
+  CartPage,
+  ErrorPage,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+  CheckoutPage,
+} from "./pages";
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="products/:id" element={<SingleProductPage />} />
+          <Route
+            path="checkout"
+            element={
+              <PrivateRoute>
+                <CheckoutPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  );
+}
+
+export default App;
